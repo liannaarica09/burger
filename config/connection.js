f6ad31e1e4ffcf0a20b10072f5a7af09e42cf19b
Maureen Bockrath <maureenbockrath@Maureens-MacBook-Air.local>
2018-05-03 01:02:25 -0700

    Jawsdb changes

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 8889,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "burger_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 8889,
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "burger_db"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
